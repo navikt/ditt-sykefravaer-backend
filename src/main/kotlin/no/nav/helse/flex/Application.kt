9ae7e7abfc619941445c5c3dc4ed9152bf92f4f7
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-04-04 12:11:19 +0200

    Legg til LeaderElection

--- src/main/kotlin/no/nav/helse/flex/Application.kt
+++ src/main/kotlin/no/nav/helse/flex/Application.kt
@@ -3,12 +3,25 @@ package no.nav.helse.flex
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.client.RestTemplate
+import java.nio.charset.StandardCharsets
 
 @SpringBootApplication
 @EnableJwtTokenValidation
 @EnableScheduling
-class Application
+class Application {
+
+    @Bean
+    fun plainTextUtf8RestTemplate(): RestTemplate {
+        return RestTemplateBuilder()
+            .messageConverters(StringHttpMessageConverter(StandardCharsets.UTF_8))
+            .build()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
--- src/main/kotlin/no/nav/helse/flex/MigrerVariantTilUpperCase.kt
+++ src/main/kotlin/no/nav/helse/flex/MigrerVariantTilUpperCase.kt
@@ -7,15 +7,21 @@ import java.util.concurrent.TimeUnit
 
 @Component
 class MigrerVariantTilUpperCase(
-    private val meldingRepository: MeldingRepository
+    private val meldingRepository: MeldingRepository,
+    private val leaderElection: LeaderElection
 ) {
 
     private val log = logger()
 
     @Scheduled(initialDelay = 3, fixedDelay = 60 * 24, timeUnit = TimeUnit.MINUTES)
     fun migrer(): Long {
-        val antallOppdatert = meldingRepository.variantTilUpperCase()
-        log.info("Endret $antallOppdatert meldinger til upper-case Variant.")
-        return antallOppdatert
+        return if (leaderElection.isLeader()) {
+            val antallOppdatert = meldingRepository.variantTilUpperCase()
+            log.info("Endret $antallOppdatert meldinger til upper-case Variant.")
+            antallOppdatert
+        } else {
+            log.info("Er ikke leader, returnerer 0.")
+            0
+        }
     }
 }
