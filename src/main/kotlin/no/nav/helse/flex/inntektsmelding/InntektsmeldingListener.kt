622446de0692ef8f986fb179dc12f501dd8036be
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-09-29 07:24:04 +0200

    devfix

--- src/main/kotlin/no/nav/helse/flex/inntektsmelding/InntektsmeldingListener.kt
+++ src/main/kotlin/no/nav/helse/flex/inntektsmelding/InntektsmeldingListener.kt
@@ -1,6 +1,10 @@
 package no.nav.helse.flex.inntektsmelding
 
+import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.flex.EnvironmentToggles
+import no.nav.helse.flex.logger
+import no.nav.helse.flex.objectMapper
+import no.nav.inntektsmeldingkontrakt.Inntektsmelding
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
@@ -12,12 +16,21 @@ class InntektsmeldingListener(
     val environmentToggles: EnvironmentToggles
 ) {
 
+    val log = logger()
+
     @KafkaListener(
         topics = [inntektsmeldingTopic],
         containerFactory = "aivenKafkaListenerContainerFactory"
     )
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
         if (environmentToggles.isProduction()) {
+            acknowledgment.acknowledge()
+            return
+        }
+        try {
+            val deserialisertIm: Inntektsmelding = objectMapper.readValue(cr.value())
+        } catch (e: Exception) {
+            log.error("Kunne ikke deserialisere inntektsmelding", e)
             acknowledgment.acknowledge()
             return
         }
