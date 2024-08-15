e21b7f538a5bec38196a91bf57f41ab38d10fcdc
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-08-15 10:10:20 +0200

    støtter svartype OPPSUMMERING

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ val tokenSupportVersion = "5.0.1"
 val logstashLogbackEncoderVersion = "8.0"
 val kluentVersion = "1.73"
 val inntektsmeldingKontraktVersion = "2024.05.21-09-56-5528e"
-val sykepengesoknadKafkaVersion = "2024.05.04-08.31-672172ee"
+val sykepengesoknadKafkaVersion = "2024.08.14-11.09-eefd3265"
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
