a161097d81a84d44e884137d1e273dec40fba8f8
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-12-16 15:24:24 +0100

    Pin kafka-native til 3.8.1
    Se https://github.com/navikt/amt-lib/pull/84

--- src/test/kotlin/no/nav/helse/flex/FellesTestOppsett.kt
+++ src/test/kotlin/no/nav/helse/flex/FellesTestOppsett.kt
@@ -49,7 +49,7 @@ abstract class FellesTestOppsett {
     companion object {
         init {
 
-            KafkaContainer(DockerImageName.parse("apache/kafka-native")).apply {
+            KafkaContainer(DockerImageName.parse("apache/kafka-native:3.8.1")).apply {
                 start()
                 System.setProperty("KAFKA_BROKERS", bootstrapServers)
             }
