2ef37f35354ad4097bc64a53cceafd275fcab64a
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2026-04-14 12:53:19 +0200

    pinner jackson versjon til 2.20.2 og setter fix-field-name-upper-case-prefix: false for å støtte den versjonen

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,9 @@ val logstashLogbackEncoderVersion = "9.0"
 val kluentVersion = "1.73"
 val inntektsmeldingKontraktVersion = "2025.04.04-01-56-365d3"
 val sykepengesoknadKafkaVersion = "2025.11.18-06.24-f860ace9"
+val jacksonVersion = "2.20.2"
+
+extra["jackson.version"] = jacksonVersion
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
