1ea3923cb881379038e1a3472eaf55359b278199
Ole Bastian Løchen <42940656+olebaba@users.noreply.github.com>
2026-04-21 11:38:05 +0200

    Oppgraderer til inntektsmelding-kontrakt versjon som støtter jackson 2.21.2 (#432)

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,11 +27,8 @@ val testContainersVersion = "2.0.4"
 val tokenSupportVersion = "5.0.39"
 val logstashLogbackEncoderVersion = "9.0"
 val kluentVersion = "1.73"
-val inntektsmeldingKontraktVersion = "2025.04.04-01-56-365d3"
+val inntektsmeldingKontraktVersion = "2026.04.15-10-22-eb2ae"
 val sykepengesoknadKafkaVersion = "2025.11.18-06.24-f860ace9"
-val jacksonVersion = "2.20.2"
-
-extra["jackson.version"] = jacksonVersion
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
